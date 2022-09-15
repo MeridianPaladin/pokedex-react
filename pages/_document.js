1b48ab087293058a3ec1684804209cd9a7ca9ff6
Victor Ramirez <vicmarama@hotmail.es>
2022-09-15 11:05:08 -0500

    Agrega app y document a configuración de next

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+        </Head>
+        <body className="bg-white">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
