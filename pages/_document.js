253e7a0da8c153639e5bb1ed3c2ce019c99ca476
Victor Ramirez <vicmarama@hotmail.es>
2022-09-15 17:58:06 -0500

    Agrega fuente personalizada en document

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,10 @@ class MyDocument extends Document {
             crossOrigin="true"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,400;0,700;0,900;1,200;1,400;1,700;1,900&display=swap"
+            rel="stylesheet"
+          ></link>
         </Head>
         <body className="bg-white">
           <Main />
