42473c628054a56a682a25846aae308fc50c77ce
Victor Ramirez <vicmarama@hotmail.es>
2022-09-16 16:35:40 -0500

    Ajusta padding de la página

--- pages/index.js
+++ pages/index.js
@@ -17,11 +17,13 @@ export default function Home() {
 
   return (
     <Layout>
-      <h1 className="mb-8 font-bold resize-text-lg">Pokedex</h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 ">
-        {pokemons.map((item, index) => (
-          <PokeCard data={item} key={index} />
-        ))}
+      <div className="p-6">
+        <h1 className="mb-8 font-bold resize-text-lg">Pokedex</h1>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 ">
+          {pokemons.map((item, index) => (
+            <PokeCard data={item} key={index} />
+          ))}
+        </div>
       </div>
     </Layout>
   );
