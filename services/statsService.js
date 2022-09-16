12dab5358b7b6790121dfd46a4e1abc6cefa1462
Victor Ramirez <vicmarama@hotmail.es>
2022-09-16 17:31:42 -0500

    Crea diccionario de estadisticas

---
+++ services/statsService.js
@@ -0,0 +1,10 @@
+export const statsDictionary = {
+  hp: "HP",
+  attack: "Attack",
+  defense: "Defense",
+  "special-attack": "Sp. Atk",
+  "special-defense": "Sp. Def",
+  speed: "Speed",
+  accuracy: "Accuracy",
+  evasion: "Evasion",
+};
