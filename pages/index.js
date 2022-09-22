796c5550da69f5a32e77bca29973c29d9ef23393
Victor Ramirez <vicmarama@hotmail.es>
2022-09-22 10:02:28 -0500

    Cambia a renderizado estatico el home

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,15 @@ import Layout from "../components/Layout";
 import PokeCard from "../components/PokeCard";
 import { getAllPokemon } from "../services/pokemonService";
 
-export default function Home() {
-  const [pokemons, setPokemons] = useState([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getAllPokemon();
-      setPokemons(data)
-      // console.log(data)
-    };
-    fetchData();
-  }, []);
+const Home = ({ pokemons }) => {
+  if (!pokemons) return <div>No pokemons found</div>;
 
   return (
     <Layout>
       <div className="p-6">
-        <h1 className="mb-8 font-bold resize-text-lg">POKEDEX by MeridianPaladin</h1>
+        <h1 className="mb-8 font-bold resize-text-lg">
+          POKEDEX by MeridianPaladin
+        </h1>
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4 ">
           {pokemons.map((item, index) => (
             <PokeCard data={item} key={index} />
@@ -27,4 +20,13 @@ export default function Home() {
       </div>
     </Layout>
   );
+};
+
+export async function getStaticProps(context) {
+  const data = await getAllPokemon();
+  return {
+    props: { pokemons: data },
+  };
 }
+
+export default Home;
