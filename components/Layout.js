ab632e795addcb80518f981c2c24c35c17f4a42a
Victor Ramirez <vicmarama@hotmail.es>
2022-09-30 18:15:18 -0500

    Ajusta tamaño de layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,12 @@
+import Footer from "./Footer";
+
 const Layout = (props) => {
-  return <div className="container h-full max-w-7xl mx-auto">{props.children}</div>;
+  return (
+    <div className="container flex flex-col h-full m-w-7xl mx-auto">
+      <div className="flex-1">{props.children}</div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
